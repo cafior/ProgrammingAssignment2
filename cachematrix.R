d2cf79b369a1da33de66f89d37b3a4af5ec1e635
cafior <83104378+cafior@users.noreply.github.com>
2022-04-16 18:38:35 -0300

    Solution to the Assignment
    Solution to the Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -3,19 +3,45 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix will create a matrix and
+## stores it in the Global Environment to be used by cacheSolve
+## In case that we need to set another matrix it will set the inverse matrix
+## to NULL to allow a new procedure via cacheSolve
+## It will also create a list with methods for get/set of both original matrix
+## and its inverse, return the list to a parent environment
+## This technique allows use of $ operator to access
+## each function from the list
 
+makeCacheMatrix <- function(x = matrix()) {
+  mInv <- NULL
+  set <- function(y) {
+    x <<- y
+    mInv <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) mInv <<- solve
+  getsolve <- function() mInv
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
 
+## cacheSolve will use x to return its inverse
+## in case that there is no x set it will allow its
+## setting in its execution
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  mInv <- x$getsolve()
+  if(!is.null(mInv)) {
+    message("getting cached data")
+    return(mInv)
+  }
+  data <- x$get()
+  mInv <- solve(data, ...)
+  x$setsolve(mInv)
+  mInv
 }
-
-## create list with methods for get/set of both original matrix
-## and its inverse, return the list to a parent environment
-## note that this technique allows use of $ operator to access
-## each function from the list
-list(set = set, get = get, setsolve = setsolve, getsolve = gestsolve)
